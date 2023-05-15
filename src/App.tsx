448f5a2f085d0759c881127526c4abeec356c7d4
sasaki <rs082283@gmail.com>
2023-05-15 10:31:54 +0900

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import DashBoard from './features/covid/DashBoard/DashBoard';
+
+function App() {
+  return (
+   <DashBoard/>
+  );
+}
+
+export default App;
